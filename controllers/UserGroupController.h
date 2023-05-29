098ac18735273ff6c1059f4e71228c547fd21427
qb <2920077696@qq.com>
2023-05-29 15:06:43 +0800

    cors

--- controllers/UserGroupController.h
+++ controllers/UserGroupController.h
@@ -1,37 +1,59 @@
 #pragma once
 
-#include <drogon/HttpController.h>
-#include <string>
-#include "models/Usergroup.h"
-#include "models/UsergroupUser.h"
-#include "models/User.h"
 #include "models/Confirm.h"
 #include "models/Transactions.h"
+#include "models/User.h"
+#include "models/Usergroup.h"
+#include "models/UsergroupUser.h"
+#include <drogon/HttpController.h>
+#include <string>
 using namespace drogon;
 using namespace drogon::orm;
 using namespace drogon_model::test;
 
-class UserGroupController : public drogon::HttpController<UserGroupController>
-{
+class UserGroupController : public drogon::HttpController<UserGroupController> {
   public:
     METHOD_LIST_BEGIN
-    
-    ADD_METHOD_TO(UserGroupController::add, "/{1}/add", Post);
-    ADD_METHOD_TO(UserGroupController::addTx, "/{1}/addTx", Post);
-    ADD_METHOD_TO(UserGroupController::cancel, "/{1}/cancel", Post);
-    ADD_METHOD_TO(UserGroupController::confirm, "/{1}/confirm", Post);
-    ADD_METHOD_TO(UserGroupController::getInfo, "/{1}/getInfo", Get);
-    ADD_METHOD_TO(UserGroupController::getConfirm, "/{1}/getConfirm", Get);
+
+    ADD_METHOD_TO(UserGroupController::add, "/{1}/add", Post, Options);
+    ADD_METHOD_TO(UserGroupController::addTx, "/{1}/addTx", Post, Options);
+    ADD_METHOD_TO(UserGroupController::cancel, "/{1}/cancel", Post, Options);
+    ADD_METHOD_TO(UserGroupController::confirm, "/{1}/confirm", Post, Options);
+    ADD_METHOD_TO(UserGroupController::getInfo, "/{1}/getInfo", Get, Options);
+    ADD_METHOD_TO(UserGroupController::getConfirm, "/{1}/getConfirm", Get, Options);
+    ADD_METHOD_TO(UserGroupController::getUsers, "/{1}/getUsers", Get, Options);
     METHOD_LIST_END
 
-    void add(const HttpRequestPtr& req, std::function<void (const HttpResponsePtr &)> &&callback, std::string uuid);
-    void addTx(const HttpRequestPtr& req, std::function<void (const HttpResponsePtr &)> &&callback, std::string uuid);
-    void cancel(const HttpRequestPtr& req, std::function<void (const HttpResponsePtr &)> &&callback, std::string uuid);
-    void confirm(const HttpRequestPtr& req, std::function<void (const HttpResponsePtr &)> &&callback, std::string uuid);
-    void getInfo(const HttpRequestPtr& req, std::function<void (const HttpResponsePtr &)> &&callback, std::string uuid);
-    void getConfirm(const HttpRequestPtr& req, std::function<void (const HttpResponsePtr &)> &&callback, std::string uuid);
-    auto getDbClient() 
-    {
-        return drogon::app().getDbClient("default");
-    }
+    void add(const HttpRequestPtr &req,
+             std::function<void(const HttpResponsePtr &)> &&callback,
+             std::string uuid);
+    bool
+    addTx_(orm::DbClientPtr &dbClientPtr,
+           std::shared_ptr<std::function<void(
+               const std::shared_ptr<drogon::HttpResponse> &)>> &callbackPtr,
+           Json::Value &tranx);
+    void addTx(const HttpRequestPtr &req,
+               std::function<void(const HttpResponsePtr &)> &&callback,
+               std::string uuid);
+    void cancel(const HttpRequestPtr &req,
+                std::function<void(const HttpResponsePtr &)> &&callback,
+                std::string uuid);
+    void confirm(const HttpRequestPtr &req,
+                 std::function<void(const HttpResponsePtr &)> &&callback,
+                 std::string uuid);
+    void getInfo(const HttpRequestPtr &req,
+                 std::function<void(const HttpResponsePtr &)> &&callback,
+                 std::string uuid);
+    void getConfirm(const HttpRequestPtr &req,
+                    std::function<void(const HttpResponsePtr &)> &&callback,
+                    std::string uuid);
+    void getUsers(const HttpRequestPtr &req,
+                    std::function<void(const HttpResponsePtr &)> &&callback,
+                    std::string uuid);
+    auto getDbClient() { return drogon::app().getDbClient("default"); }
+    bool
+    confirm_(orm::DbClientPtr &dbClientPtr,
+             std::shared_ptr<std::function<void(
+                 const std::shared_ptr<drogon::HttpResponse> &)>> &callbackPtr,
+             const std::string &uuid, const std::string &txuuid);
 };
--- main.cc
+++ main.cc
@@ -4,6 +4,14 @@ int main() {
     // drogon::app().addListener("0.0.0.0",13456);
     //Load config file
     drogon::app().loadConfigFile("../config.json");
+    std::string corsValue = drogon::app().getCustomConfig()["cors"].asString();
+    LOG_DEBUG << corsValue;
+    drogon::app()
+    	.registerPostHandlingAdvice(
+    		[&](const drogon::HttpRequestPtr &req, const drogon::HttpResponsePtr &resp) {
+            //LOG_DEBUG << "postHandling1";
+            resp->addHeader("Access-Control-Allow-Origin", corsValue);
+        });
     //Run HTTP framework,the method will block in the internal event loop
     drogon::app().run();
     return 0;
