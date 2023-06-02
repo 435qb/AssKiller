b07e1ce36e0c5451cff22ce577b09a08f4662e01
qb <2920077696@qq.com>
2023-06-02 21:26:58 +0800

    add config path

--- main.cc
+++ main.cc
@@ -1,9 +1,9 @@
 #include <drogon/drogon.h>
-int main() {
+int main(int argc, char *argv[]) {
     //Set HTTP listener address and port
     // drogon::app().addListener("0.0.0.0",13456);
     //Load config file
-    drogon::app().loadConfigFile("../config.json");
+    drogon::app().loadConfigFile(argv[1]);
     std::string corsValue = drogon::app().getCustomConfig()["cors"].asString();
     LOG_DEBUG << corsValue;
     drogon::app()
